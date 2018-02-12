490e7c8a2d310fa86957b233d07286221df1460d
Athena Ozanich <athenaozanich@gmail.com>
2018-02-12 13:56:18 -0500

    Commit via web interface due to git errors.

--- js/customize-colorbox.js
+++ js/customize-colorbox.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 				//Examples of how to assign the Colorbox event to elements
-				$(".group1").colorbox({rel:'group1', width:"60%", height:"75%", transition:"fade"});
+				$(".group1").colorbox({rel:'group1', width:"80%", height:"550px", transition:"fade", speed:1000, slideshow:true});
 				$(".group2").colorbox({rel:'group2', transition:"fade"});
 				$(".group3").colorbox({rel:'group3', transition:"none", width:"75%", height:"75%"});
 				$(".group4").colorbox({rel:'group4', slideshow:true});
--- js/search-filter.js
+++ js/search-filter.js
@@ -1,8 +1,8 @@
-$('#search').change(function() {
-     var searchStr = $(this).val();
+$('#search').bind('input', function() {
+     var searchStr = $(this).val().toLowerCase();
      $('.image').each(function() {
-          var str = $(this).attr("title");
-          var str2 = $(this).attr("caption");
+          var str = $(this).attr("alt").toLowerCase();
+          var str2 = $(this).attr("caption").toLowerCase();
 
           if(str.indexOf(searchStr) > -1) { //compares search text to title text
                $(this).show(); // if the entry is found show the container
